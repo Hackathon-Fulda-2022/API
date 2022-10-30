0fbbaa00e66e0dac11537bc112b43fa44b0eb4f4
MRV1N2 <84151289+MRV1N2@users.noreply.github.com>
2022-10-30 06:59:06 +0100

    Correction in api_lib.py

--- api_lib.py
+++ api_lib.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
-import re
-import time
-from datetime import datetime, timezone
-import pandas as pd
+from init_database import *
+from example_data import *
 import requests
 import json
 
-from example_data import *
-
 
 class hackathon_api:
-    #__api_path = "https://crypticorn-api.herokuapp.com/"
-    __api_path = "http://127.0.0.1:5000/"
+    # Link to the deployed API on heroku Server
+    __api_path = "https://hackathon2022fulda.herokuapp.com/"
+    # Link to to a local running API
+    #__api_path = "http://127.0.0.1:5000/"
 
 
     def post_initialize_vitalsTypes(self):
+        '''
+        Initialze the constant vitalsTypes table.
+        :return: api response status code
+        '''
 
         response = requests.post(
             self.__api_path + 'post_initialize_vitalsTypes',
-            #headers={'Authorization': "Bearer " + self.__access_token},
-            # json={
-            #    "are_you_sure": are_you_sure
-            # }
         )
         return json.loads(response.content)['status']
 
     def post_initialize_rooms(self, rooms_dict):
+        '''
+        Initialize a new constant room in the facility.
+        :param rooms_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        '''
 
         response = requests.post(
             self.__api_path + 'post_initialize_rooms',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                 "rooms_dict": rooms_dict
             }
@@ -38,43 +40,60 @@ class hackathon_api:
         return json.loads(response.content)['status']
 
     def post_new_patient(self, patient_dict):
+        """
+        Adds new partient.
+        :param rooms_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_new_patient',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                 "patient_dict": patient_dict
             }
         )
         return json.loads(response.content)['status']
 
-    def post_new_employee(self, employee):
+    def post_new_employee(self, employee_dict):
+        """
+        Adds an employee, at the beginning of his/her work sift.
+        :param employee_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_new_employee',
             #headers={'Authorization': "Bearer " + self.__access_token},
             json={
-               "employee": employee
+               "employee": employee_dict
             }
         )
         return json.loads(response.content)['status']
 
     def post_new_prescriptions(self, prescriptions_dict):
+        """
+        Adds new prescriptions from the doc.
+        :param rooms_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_new_prescriptions',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
-               "prescriptions_dict": prescriptions_dict
+               "rooms_dict": prescriptions_dict
             }
         )
         return json.loads(response.content)['status']
 
     def post_new_patientRequest(self, patientRequest_dict):
+        """
+        Adds new request from the partient to AI Anna or the employees.
+        :param patientRequest_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_new_patientRequest',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                 "patientRequest_dict": patientRequest_dict
              }
@@ -82,10 +101,14 @@ class hackathon_api:
         return json.loads(response.content)['status']
 
     def post_update_medication(self, medication_dict):
+        """
+        If the patien gets medication, this function will be called
+        :param medication_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_update_medication',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                "medication_dict": medication_dict
             }
@@ -93,10 +116,14 @@ class hackathon_api:
         return json.loads(response.content)['status']
 
     def post_update_vitals(self, vitals_dict):
+        """
+        Update the vitals from speech or from meassurement devices.
+        :param vitals_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_update_vitals',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                "vitals_dict": vitals_dict
             }
@@ -104,10 +131,14 @@ class hackathon_api:
         return json.loads(response.content)['status']
 
     def post_update_patientcondition(self, patientcondition_dict):
+        """
+        Only text based information.
+        :param patientcondition_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_update_patientcondition',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                 "patientcondition_dict": patientcondition_dict
             }
@@ -115,10 +146,14 @@ class hackathon_api:
         return json.loads(response.content)['status']
 
     def post_update_roomConditions(self, roomConditions_dict):
+        """
+        Updates room Conditions from measurement devices
+        :param roomConditions_dict: special configured dictionary for this api endpoint
+        :return: api response status code
+        """
 
         response = requests.post(
             self.__api_path + 'post_update_roomConditions',
-            #headers={'Authorization': "Bearer " + self.__access_token},
             json={
                "roomConditions_dict": roomConditions_dict
             }
@@ -128,14 +163,15 @@ class hackathon_api:
 
 if __name__ == '__main__':
 
+    # Example Usage
     api = hackathon_api()
     #api.post_initialize_vitalsTypes()
     #api.post_new_patient(patient_dict)
     #api.post_new_patientRequest(patientRequest_dict)
-    #api.post_update_vitals(vitals_dict)
+    api.post_update_vitals(vitals_dict)
     #api.post_update_patientcondition(patientcondition_dict)
     #api.post_new_prescriptions(prescriptions_dict)
     #api.post_update_medication(medication_dict)
-    api.post_initialize_rooms(room_dict)
-    api.post_update_roomConditions(roomConditions_dict)
-    print()
+    #api.post_initialize_rooms(room_dict)
+    #api.post_update_roomConditions(roomConditions_dict)
+    print('Done')
